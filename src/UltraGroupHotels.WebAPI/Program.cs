25528d1f66cd5d18f4828d111ae070c3c80f12b7
Roger Roldan <roger.roldan@sasoftco.com>
2024-12-17 01:10:30 -0500

    update project ci/cd configuration github

--- src/UltraGroupHotels.WebAPI/Program.cs
+++ src/UltraGroupHotels.WebAPI/Program.cs
@@ -15,10 +15,11 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
     app.ApplyMigration();
 }
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseExceptionHandler("/error");
 
